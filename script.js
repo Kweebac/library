e08e517d5aa7db98e59b745b62dda22f32de8daf
Kweebac <06.amarshall@gmail.com>
2023-04-17 16:46:46 +0100

    Added basic visuals & function to create book objects

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-let test = 5;
+let library = [];
+
+function Book(title, author, pages, finished) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.finished = finished;
+}
+
+function addBook(title, author, pages, finished) {
+  library.push(new Book(title, author, pages, finished));
+}
+
+addBook("The Return of the King", "J.R.R Tolkien", 416, true);
+addBook("Book1", "Rando", 2123, true);
+addBook("Book2", "Rando", 32, false);
+addBook("Book3", "Rando", 124, false);
+addBook("Book4", "Rando", 555);
